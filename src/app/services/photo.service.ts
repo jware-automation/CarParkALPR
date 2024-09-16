f1e5ffbc44dfcf678f95df60bee16284cd7f4d73
Janith Bandara of jWare Automation <janith@jwareautomation.com>
2024-09-16 23:31:48 +0530

    working

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
+import { Platform } from '@ionic/angular';
 import { OpenALPR, OpenALPROptions, OpenALPRResult } from '@awesome-cordova-plugins/openalpr/ngx';
 
 @Injectable({
@@ -10,20 +11,24 @@ import { OpenALPR, OpenALPROptions, OpenALPRResult } from '@awesome-cordova-plug
 export class PhotoService {
   public photos: UserPhoto[] = [];
   public openALPRResults: OpenALPRResult[] = [];
+  private platform: Platform;
 
-  constructor(private openALPR: OpenALPR) { }
+  constructor(platform: Platform, private openALPR: OpenALPR) {
+    this.platform = platform;
+  }
 
   public async addNewToGallery() {
     // Take a photo
     const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Base64,
+      resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
     });
 
     let res = '';
+    const base64Data = await this.readAsBase64(capturedPhoto);
 
-    this.openALPR.scan(capturedPhoto.base64String, {
+    this.openALPR.scan(base64Data, {
       country: this.openALPR.Country.US,
       amount: 3
     }).then((result: [OpenALPRResult]) => {
@@ -59,6 +64,34 @@ export class PhotoService {
 
       });
   }
+
+  private async readAsBase64(photo: Photo) {
+    // "hybrid" will detect Cordova or Capacitor
+    if (this.platform.is('hybrid')) {
+      // Read the file into base64 format
+      const file = await Filesystem.readFile({
+        path: photo.path!
+      });
+
+      return file.data;
+    }
+    else {
+      // Fetch the photo, read as a blob, then convert to base64 format
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+
+      return await this.convertBlobToBase64(blob) as string;
+    }
+  }
+
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
 
 export interface UserPhoto {
